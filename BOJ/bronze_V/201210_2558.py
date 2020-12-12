424b6d5d4e9fbd4fef2dd046882b50e9dddbad7c
lcg0826 <lightchan0826@gmail.com>
2020-12-12 23:09:01 +0900

    백준 2558 A+B

---
+++ BOJ/bronze_V/201210_2558.py
@@ -0,0 +1,23 @@
+# https://www.acmicpc.net/problem/2558
+n = int(input())
+m = int(input())
+
+print(n+m)
+
+# # Java로 할 경우
+# import java.util.Scanner;
+# public class Main {
+# 
+#     public static void main(String[] args) {
+#         Scanner sc = new Scanner(System.in);
+#         int A = sc.nextInt();
+#         int B = sc.nextInt();
+# 
+#         
+#         System.out.println(A+B);
+# 
+#         
+# 
+#     }
+# 
+# }
