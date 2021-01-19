4159c0c076015725703ce0f42457f9c5599531bf
lcg0826 <lightchan0826@gmail.com>
2021-01-19 22:57:11 +0900

    BOJ 1934 '최소공배수'

---
+++ BOJ/Sliver_V/210119_1934.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/1934
+# 최소공배수
+
+def min(n, m):
+    result = max(n, m)
+    return result * (n // result) * (m // result)
+def max(n, m):
+    return max(m % n, n) if m % n else n
+
+num = int(input())
+for i in range(num):
+    num1, num2 = map(int, input().split())
+    print(min(num1, num2))
