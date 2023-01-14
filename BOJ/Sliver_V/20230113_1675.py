3c765cd58b89eab7e03e141e7b2bc3ebb052a0d7
이찬규 <lcg082695@gmail.com>
2023-01-14 21:11:55 +0900

    1일 1백준 코드

---
+++ BOJ/Sliver_V/20230113_1675.py
@@ -0,0 +1,12 @@
+n = int(input())
+card = [0] * 10
+for i in str(n):
+    if i == "6" or i == "9":
+        if card[6] == card[9]:
+            card[6] += 1
+        else:
+            card[9] += 1
+    else:
+        card[int(i)] += 1
+
+print(max(card))
