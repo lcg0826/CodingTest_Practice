2537c3bc7e1af028062f3739c7ea44b4513452ca
이찬규 <lcg082695@gmail.com>
2023-03-16 01:13:59 +0900

    1일 1일 알골

---
+++ Programmers/Lv.1/나머지가 1이 되는 수 찾기.py
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/87389
+
+def solution(n):
+    answer = 0;
+    for i in range(1, n+1):
+        if n % i == 1:
+            answer = i;
+            break;
+    return answer
